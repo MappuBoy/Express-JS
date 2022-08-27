e02296e75abe63271c4a69d7d89ff11f29d49061
mappuboy <pituvaraliyange@gmail.com>
2022-08-27 16:14:30 +0530

    mysql connector

--- routes/customer.js
+++ routes/customer.js
@@ -1,3 +1,18 @@
 const express=require('express')
 const router=express.Router()
 const mysql=require('mysql')
+const db=require('../config/db.config')
+
+const connection=mysql.createConnection(db.database)
+connection.connect(function(err){
+    if(err){
+        console.log(err);
+    }else{
+        var customertable="CREATE TABLE IF NOT EXISTS customer(id varchar(255),name varchar(255))"
+    connection.query(customertable,function(err,result){
+        console.log("Connect database");
+    })
+    }
+})
+module.exports=router
+
